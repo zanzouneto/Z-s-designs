da81766931550561db4bc867a6e7a7e14c57787c
Zeina Saade <saadezeina@hotmail.com>
2023-10-18 14:38:54 +0200

    2nd commit

--- index.js
+++ index.js
@@ -21,11 +21,49 @@ document.addEventListener('mousemove', function(e) {
         document.getElementById('backgroundB').style.opacity = 1 - opacityA; // Adjust backgroundB's opacity based on backgroundA's
     }
     
-        // Resize cursor
-
-        let distanceToCenter = Math.sqrt(Math.pow(width/2 - x, 2) + Math.pow(height/2 - y, 2));
-        let maxDistance = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
-        let cursorSize = Math.min(128, 16 + (16 * (1 - (distanceToCenter / maxDistance))));
-        document.body.style.cursor = `url('assets/pointer.png') ${cursorSize} ${cursorSize}, none`;
-
 });
+document.addEventListener('click', function(e) {
+    let bgA = document.getElementById('backgroundA');
+    let bgB = document.getElementById('backgroundB');
+
+    if (window.getComputedStyle(bgA).opacity === "1") {
+        window.open('cv.html', '_blank');
+    } else if (window.getComputedStyle(bgB).opacity === "1") {
+        window.location.href = 'newslider.html';
+    }
+});
+const cursor = document.getElementById('custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+    // Update the position of the custom cursor
+    cursor.style.left = e.pageX - 16 + 'px';
+    cursor.style.top = e.pageY - 16 + 'px';
+
+    // Scale it up
+    cursor.style.transform = 'scale(1.3)';
+});
+
+document.addEventListener('mouseout', () => {
+    cursor.style.transform = 'scale(1)';
+});
+
+document.addEventListener('mouseenter', () => {
+    cursor.style.transform = 'scale(1.3)';
+});
+
+// Return to original size after a delay
+document.addEventListener('mousemove', debounce(() => {
+    cursor.style.transform = 'scale(1)';
+}, 150));
+
+// A simple debounce function
+function debounce(func, wait) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(this, args);
+        }, wait);
+    };
+}
+
