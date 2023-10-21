d86bb7b8e4212cf1fd846843e47e350444cbd483
Zeina Saade <saadezeina@hotmail.com>
2023-10-22 01:20:21 +0200

    phptest

--- navigation.php
+++ navigation.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
