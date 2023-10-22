779027e31262187247c135ebd4e3c4da34207889
Zeina Saade <saadezeina@hotmail.com>
2023-10-22 15:19:22 +0200

    mobile optimisation

--- project/project.js
+++ project/project.js
@@ -1,18 +1,24 @@
 function reveal() {
-    var reveals = document.querySelectorAll("section");
-  
-    for (var i = 0; i < reveals.length; i++) {
+  var reveals = document.querySelectorAll("section");
+
+  for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
+
+      // Default value for larger screens
       var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
+
+      // If screen width is less than or equal to 768px (common breakpoint for mobile)
+      if (window.innerWidth <= 768) {
+          elementVisible = 50; // Adjust this value to suit your requirements
       }
-    }
+
+      if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+      } else {
+          reveals[i].classList.remove("active");
+      }
   }
-  
-  window.addEventListener("scroll", reveal);
-  
+}
+
+window.addEventListener("scroll", reveal);
